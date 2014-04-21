b7ac66cb17f1cb0ad94835c8e77526075372d718
Chunyih Hsu <brandon.cyh@gmail.com>
2014-04-20 17:59:54 -0700

    heroku fix

--- web.js
+++ web.js
@@ -5,4 +5,5 @@ var app = express();
  
 app.use(logfmt.requestLogger());
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.listen(process.env.PORT || 5000);
